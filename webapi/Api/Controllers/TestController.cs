65a3fd50e04da636c56df4ab2b0af0db3a3a5a18
Mateusz Rozpara <mat.rozpara@gmail.com>
2022-02-09 23:48:04 +0100

    Add MediatR

---
+++ webapi/Api/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Handlers.Test;
+using System.Threading;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public TestController(IMediator mediator, ILogger<TestController> logger)
+        {
+            _mediator = mediator;
+        }
+
+
+
+        [HttpGet]
+        public async Task<int> Get()
+        {
+            await _mediator.Send(new TestQuery(), CancellationToken.None);
+
+            return await Task.FromResult(1);
+        }
+    }
+}
---
+++ webapi/Application/Handlers/Test/TestQuery.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Test
+{
+    public class TestQuery : IRequest
+    {
+    }
+
+    public class TestQueryHandler : IRequestHandler<TestQuery>
+    {
+        public Task<Unit> Handle(TestQuery request, CancellationToken cancellationToken)
+        {
+            string a = "test";
+
+            throw new NotImplementedException();
+        }
+    }
+}
